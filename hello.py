b81e8bc28094c2370852790e8492cdeb36222480
De3Zy <89665316+DominikZzZ@users.noreply.github.com>
2022-12-22 17:11:44 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,15 +8,7 @@ char_W: list = []; [char_W.append(1) for _ in range(1, ord("W") + 1)]
 char_r: list = []; [char_r.append(1) for _ in range(1, ord("r") + 1)]
 char_d: list = []; [char_d.append(1) for _ in range(1, ord("d") + 1)]
 
+helloworld: list = [char_H, char_e, char_l, char_l, char_o, char_space, char_W, char_o, char_r, char_l, char_d]
 
-print(chr(sum(char_H)))
-print(chr(sum(char_e)))
-print(chr(sum(char_l)))
-print(chr(sum(char_l)))
-print(chr(sum(char_o)))
-print(chr(sum(char_space)))
-print(chr(sum(char_W)))
-print(chr(sum(char_o)))
-print(chr(sum(char_r)))
-print(chr(sum(char_l)))
-print(chr(sum(char_d)))
+for i in helloworld:
+    print(chr(sum(i)), end="")
